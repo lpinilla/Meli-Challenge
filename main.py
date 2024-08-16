4595c703df17f62d03bded3baedbcfa5c5a032bc
lpinilla <lautaro.nipi@gmail.com>
2024-08-15 23:24:45 -0300

    feat: poc ingesta y validacion de datos de db ok

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import os
 import csv
 import json
-from models.models import Employee, Base
+from types import SimpleNamespace
+from models.models import Employee, Base, DBInfo, DBClass
 from sqlalchemy import create_engine
 from sqlalchemy.orm import relationship, sessionmaker
 
@@ -15,16 +16,42 @@ db = create_engine("postgresql://%s:%s@%s:%s/%s" % (DB_USER, DB_PASS, DB_HOST, D
 Session = sessionmaker(bind=db)
 Base.metadata.create_all(db)
 
+session = Session()
 
 employees = []
+db_info = []
 
 #read csv
 with open('employee_data.csv', encoding='utf-8') as f:
-        csvReader = csv.DictReader(f)
-        for r in csvReader:
-            employees.append(Employee(int(r['user_id']), bool(r['user_state']), str(r['user_mail']), int(r['user_manager'])))
-            session = Session()
-            session.add_all(employees)
-            session.commit()
-            session.close()
+    csvReader = csv.DictReader(f)
+    for r in csvReader:
+        employees.append(Employee(int(r['user_id']), bool(r['user_state']), str(r['user_mail']), int(r['user_manager'])))
+
+session.add_all(employees)
+
+def validate_db_data(db_info):
+    required_fields = {'db_name': str, 'owner_id': int, 'classification': int}
+    for key, expected_type in required_fields.items():
+        if key not in db_info or not isinstance(db_info[key], expected_type):
+            return False
+    return True
+
+raw_data = json.load(open('dbs_data.json', encoding='utf-8'))
+valid_entries = []
+invalid_entries = []
+for rd in raw_data:
+    if validate_db_data(rd):
+        valid_entries.append(
+                DBInfo(
+                    str(rd['db_name']),
+                    int(rd['owner_id']),
+                    DBClass(rd['classification'])
+                )
+        )
+    else:
+        invalid_entries.append(rd)
+
+session.add_all(valid_entries)
+session.commit()
+session.close()
 

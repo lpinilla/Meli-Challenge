5eb2a1017b5b9bc5991cb2c44af248c7fdaa3306
lpinilla <lautaro.nipi@gmail.com>
2024-08-16 20:46:03 -0300

    feat: creados endpoints /csv y /json para recibir los archivos

--- main.py
+++ main.py
@@ -1,28 +1,36 @@
-import os
 import csv
 import json
+from io import StringIO
 from types import SimpleNamespace
-from models import ModelBase as Base
 from models.employee import Employee
 from models.db_info import DBInfo, DBClass
-from fastapi import Depends, FastAPI, HTTPException
+from fastapi import Depends, FastAPI, HTTPException, File, UploadFile
+from database import Base, Session, engine
 
-Session = sessionmaker(bind=engine)
 Base.metadata.create_all(engine)
 app = FastAPI()
-#session = Session()                                                          
-                                                                              
-employees = []                                                                
-db_info = []                                                                  
-                                                                              
-#read csv                                                                     
-with open('employee_data.csv', encoding='utf-8') as f:                        
-    csvReader = csv.DictReader(f)
+
+def get_db():
+    db = Session()
+    try:
+        yield db
+    finally:
+        db.close()
+
+#Endpoint to upload csv of employees
+@app.post('/csv')
+async def upload_csv(file: UploadFile = File(...), db: Session = Depends(get_db)):
+    #assumes this file is correct
+    content = await file.read()
+    csv_raw = StringIO(content.decode('utf-8'))
+    csvReader = csv.DictReader(csv_raw)
+    employees = []
     for r in csvReader:
         employees.append(Employee(int(r['user_id']), bool(r['user_state']), str(r['user_mail']), int(r['user_manager'])))
+    db.add_all(employees)
+    db.commit()
 
-session.add_all(employees)
-
+#basic validation: all fields are correct
 def validate_db_data(db_info):
     required_fields = {'db_name': str, 'owner_id': int, 'classification': int}
     for key, expected_type in required_fields.items():
@@ -30,22 +38,23 @@ def validate_db_data(db_info):
             return False
     return True
 
-raw_data = json.load(open('dbs_data.json', encoding='utf-8'))
-valid_entries = []
-invalid_entries = []
-for rd in raw_data:
-    if validate_db_data(rd):
-        valid_entries.append(
-                DBInfo(
-                    str(rd['db_name']),
-                    int(rd['owner_id']),
-                    DBClass(rd['classification'])
-                )
-        )
-    else:
-        invalid_entries.append(rd)
 
-session.add_all(valid_entries)
-session.commit()
-session.close()
+#Endpoint to upload json with db data, data *can* be corrupted
+@app.post('/json')
+async def upload_json(file: UploadFile = File(...), db: Session = Depends(get_db)):
+    content = await file.read()
+    json_raw = json.loads(content)
+    valid_entries = []
+    invalid_entries = []
+    for rd in json_raw:
+        if validate_db_data(rd):
+            valid_entries.append(
+                    DBInfo(str(rd['db_name']),int(rd['owner_id']),DBClass(rd['classification']))
+            )
+        else:
+            invalid_entries.append(rd)
+    db.add_all(valid_entries)
+    db.commit()
+    db.close()
+    return invalid_entries
 
